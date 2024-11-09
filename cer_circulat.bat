74df300140fee88ad5ea862551839e0a7c588731
123 <il.v.matveev@yandex.ru>
2024-11-09 11:58:03 +0300

    цикл успешный

--- cer_circulat.bat
+++ cer_circulat.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 setlocal enabledelayedexpansion
 
+set "place=C:\Users\123\windows"
 set "path_input_cer=C:\Users\123\windows\enc\*.cer_enc"
 set "path_input_key=C:\Users\123\windows\enc\*.key_enc"
 set "path_out=!path_cert_enc!\enc_out_circulat"
@@ -10,13 +11,13 @@ set "list=!place!\enc_circulat.list"
 mkdir !enc_out_circulat! 2> NUL
 
 :: формирование формата времени для записи в лог истории скрипта
-set datetimef=%date:~-4%-%date:~3,2%-%date:~0,2%  %time:~0,2%:%time:~3,2%:%time:~6,2%
-echo %datetimef% >> !log!
+set datetime=%date:~-4%-%date:~3,2%-%date:~0,2%  %time:~0,2%:%time:~3,2%:%time:~6,2%
+echo !datetime! >> !log!
 
-for %%f in (!list!) do (
+
+for /F %%f in (!list!) do (
   set "file_name=%%f"
-  type %%f >> !log!
-  )
+  echo %%f >> !log!
   
   set "cer_name=!path_out!\!file_name!.cer_enc"
   set "key_name=!path_out!\!file_name!.key_enc"
@@ -29,6 +30,7 @@ for %%f in (!list!) do (
   
   
   )
-  
+echo '------------' >> !log!
+exit  
   
 :: if ююю (echo !file_name! >> !log!)  else (echo "-" >> !log!) 
